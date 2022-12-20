1586553a2ea1d0b1fe476e9c34276433f9fbf76a
Chenhao Ye <ye.chenhao.ych@gmail.com>
2022-12-19 18:18:51 -0700

    [MINOR] formatting

--- scripts/run.py
+++ scripts/run.py
@@ -11,21 +11,25 @@ PROG_BUILD_DISK_INDEX = f"{DISKANN_DIR}/build/tests/build_disk_index"
 PROG_DISK_INDEX_TO_TENSORS = f"{DISKANN_DIR}/build/tests/utils/disk_index_to_tensors"
 PROG_SEARCH_DISK_INDEX = f"{DISKANN_DIR}/build/tests/search_disk_index"
 
+
 def check_file_exists(path):
     if not os.path.isfile(path):
         print(f"file {path} does not exist", file=sys.stderr)
         exit(1)
+
 
 def check_dir_exists(path):
     if not os.path.isdir(path):
         print(f"directory {path} does not exist", file=sys.stderr)
         exit(1)
 
+
 def run_program(program, options):
     check_file_exists(program)
     cmd = [program] + options
     print("\n=== Running ===", ' '.join(cmd))
     subprocess.run(cmd, check=True)
+
 
 def handle_to_fbin(sift_base, dataset, max_npts):
     learn_fvecs_path = f"{sift_base}_learn.fvecs"
@@ -43,14 +47,19 @@ def handle_to_fbin(sift_base, dataset, max_npts):
     options = [query_fvecs_path, query_fbin_path]
     run_program(PROG_FVECS_TO_FBIN, options)
 
+
 def handle_build(dataset):
     learn_fbin_path = f"{dataset}_learn.fbin"
     index_path_prefix = f"{dataset}_R32_L50_A1.2"
     check_file_exists(learn_fbin_path)
 
-    options = ['--data_type', 'float', '--dist_fn', 'l2', '--data_path', learn_fbin_path,
-               '--index_path_prefix', index_path_prefix, '-R', '32', '-L', '50', '-B', '0.003', '-M', '1']
+    options = [
+        '--data_type', 'float', '--dist_fn', 'l2', '--data_path',
+        learn_fbin_path, '--index_path_prefix', index_path_prefix, '-R', '32',
+        '-L', '50', '-B', '0.003', '-M', '1'
+    ]
     run_program(PROG_BUILD_DISK_INDEX, options)
+
 
 def handle_convert(dataset):
     disk_index_path = f"{dataset}_R32_L50_A1.2_disk.index"
@@ -60,7 +69,9 @@ def handle_convert(dataset):
     options = ['float', disk_index_path, tensors_prefix]
     run_program(PROG_DISK_INDEX_TO_TENSORS, options)
 
-def handle_query(dataset, k_depth, npts_to_cache, use_ts, ts_async, list_sizes):
+
+def handle_query(dataset, k_depth, npts_to_cache, use_ts, ts_async,
+                 list_sizes):
     index_path_prefix = f"{dataset}_R32_L50_A1.2"
     disk_index_path = f"{dataset}_R32_L50_A1.2_disk.index"
     tensors_prefix = f"{dataset}_R32_L50_A1.2_tensor"
@@ -73,9 +84,13 @@ def handle_query(dataset, k_depth, npts_to_cache, use_ts, ts_async, list_sizes):
     check_dir_exists(f"{tensors_prefix}_num_nbrs.zarr")
     check_dir_exists(f"{tensors_prefix}_nbrhood.zarr")
 
-    options = ['--data_type', 'float', '--dist_fn', 'l2', '--index_path_prefix', index_path_prefix,
-               '--query_file', query_fbin_path, '--gt_file',  gt_file_path, '-K', str(k_depth),
-               '--result_path', res_path_prefix, '--num_nodes_to_cache', str(npts_to_cache), '-L']
+    options = [
+        '--data_type', 'float', '--dist_fn', 'l2', '--index_path_prefix',
+        index_path_prefix, '--query_file', query_fbin_path, '--gt_file',
+        gt_file_path, '-K',
+        str(k_depth), '--result_path', res_path_prefix, '--num_nodes_to_cache',
+        str(npts_to_cache), '-L'
+    ]
     for l in list_sizes:
         options.append(str(l))
     if use_ts:
@@ -84,28 +99,68 @@ def handle_query(dataset, k_depth, npts_to_cache, use_ts, ts_async, list_sizes):
             options.append('--use_tensors_async')
     run_program(PROG_SEARCH_DISK_INDEX, options)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Driver of DiskANN tests")
-    subparsers = parser.add_subparsers(help="Supported commands", dest='subparser', required=True)
+    subparsers = parser.add_subparsers(help="Supported commands",
+                                       dest='subparser',
+                                       required=True)
 
-    parser_to_fbin = subparsers.add_parser('to_fbin', help="generate input binary from sift fvecs")
-    parser_to_fbin.add_argument('--sift_base', help="input prefix, should be the prefix <this>_learn.fvecs", required=True)
-    parser_to_fbin.add_argument('--dataset', help="dataset name, will be the prefix <this>_learn.fbin", required=True)
-    parser_to_fbin.add_argument('--max_npts', help="#points to extract (if want smaller dataset)", type=int, default=0)
-    
-    parser_build = subparsers.add_parser('build', help="build on-disk index from fbin file")
-    parser_build.add_argument('--dataset', help="dataset name, should be the prefix <this>_learn.fbin", required=True)
+    parser_to_fbin = subparsers.add_parser(
+        'to_fbin', help="generate input binary from sift fvecs")
+    parser_to_fbin.add_argument(
+        '--sift_base',
+        help="input prefix, should be the prefix <this>_learn.fvecs",
+        required=True)
+    parser_to_fbin.add_argument(
+        '--dataset',
+        help="dataset name, will be the prefix <this>_learn.fbin",
+        required=True)
+    parser_to_fbin.add_argument(
+        '--max_npts',
+        help="#points to extract (if want smaller dataset)",
+        type=int,
+        default=0)
 
-    parser_convert = subparsers.add_parser('convert', help="convert disk index to zarr format tensors")
-    parser_convert.add_argument('--dataset', help="dataset name, should be the prefix <this>_learn.fbin", required=True)
+    parser_build = subparsers.add_parser(
+        'build', help="build on-disk index from fbin file")
+    parser_build.add_argument(
+        '--dataset',
+        help="dataset name, should be the prefix <this>_learn.fbin",
+        required=True)
 
-    parser_query = subparsers.add_parser('query', help="run query (search) on index in various modes")
-    parser_query.add_argument('--dataset', help="dataset name, should be the prefix <this>_learn.fbin", required=True)
-    parser_query.add_argument('--k_depth', help="how many nearest neighbors to query", type=int, default=10)
-    parser_query.add_argument('--npts_to_cache', help="#points to cache ahead of time", type=int, default=10000)
-    parser_query.add_argument('--use_ts', action='store_true', help="use tensorstore backend")
-    parser_query.add_argument('--ts_async', action='store_true', help="use tensorstore in async mode")
-    parser_query.add_argument('--list_sizes', help="list of search list sizes", metavar='L', type=int, nargs='+',
+    parser_convert = subparsers.add_parser(
+        'convert', help="convert disk index to zarr format tensors")
+    parser_convert.add_argument(
+        '--dataset',
+        help="dataset name, should be the prefix <this>_learn.fbin",
+        required=True)
+
+    parser_query = subparsers.add_parser(
+        'query', help="run query (search) on index in various modes")
+    parser_query.add_argument(
+        '--dataset',
+        help="dataset name, should be the prefix <this>_learn.fbin",
+        required=True)
+    parser_query.add_argument('--k_depth',
+                              help="how many nearest neighbors to query",
+                              type=int,
+                              default=10)
+    parser_query.add_argument('--npts_to_cache',
+                              help="#points to cache ahead of time",
+                              type=int,
+                              default=10000)
+    parser_query.add_argument('--use_ts',
+                              action='store_true',
+                              help="use tensorstore backend")
+    parser_query.add_argument('--ts_async',
+                              action='store_true',
+                              help="use tensorstore in async mode")
+    parser_query.add_argument('--list_sizes',
+                              help="list of search list sizes",
+                              metavar='L',
+                              type=int,
+                              nargs='+',
                               default=('10', '50', '100'))
 
     args = parser.parse_args()
@@ -116,5 +171,5 @@ if __name__ == "__main__":
     elif args.subparser == "convert":
         handle_convert(args.dataset)
     elif args.subparser == "query":
-        handle_query(args.dataset, args.k_depth, args.npts_to_cache, args.use_ts, args.ts_async,
-                     args.list_sizes)
+        handle_query(args.dataset, args.k_depth, args.npts_to_cache,
+                     args.use_ts, args.ts_async, args.list_sizes)
